4a0bfe33d5184a3c5e8edd4ca4d12be44ac186dc
Abdul Rehman <a.rehman.nazar@gmail.com>
2023-10-06 11:34:36 +0500

    Add visibility method to show/Hide the view

--- ComonUtility/src/main/java/com/ar/coomonutillib/ui/UiUtil.kt
+++ ComonUtility/src/main/java/com/ar/coomonutillib/ui/UiUtil.kt
@@ -1,6 +1,7 @@
 package com.ar.coomonutillib.ui
 
 import android.content.Context
+import android.view.View
 import android.widget.Toast
 
 
@@ -11,8 +12,24 @@ import android.widget.Toast
 
 class UiUtil {
     companion object {
+        /**
+         * Show Toast
+         */
         fun showToast(context: Context, msg: String) {
             Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+        }
+
+        /**
+         * Utility method to set the visibility of view
+         * @param view
+         * @param visible  if true then show other hide the view
+         */
+        fun setViewVisibility(view: View, visible: Boolean) {
+            if (visible) {
+                view.visibility = View.VISIBLE
+            } else {
+                view.visibility = View.GONE
+            }
         }
     }
 }
