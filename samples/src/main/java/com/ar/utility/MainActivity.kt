7a506cc3064390ab09e04128bd5f406a01dad286
Abdul Rehman <a.rehman.nazar@gmail.com>
2024-01-03 19:22:32 +0500

    Add Extensions function instead of class functions

--- samples/src/main/java/com/ar/utility/MainActivity.kt
+++ samples/src/main/java/com/ar/utility/MainActivity.kt
@@ -3,14 +3,23 @@ package com.ar.utility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.ar.coomonutillib.ui.ARUiUtil
+import com.ar.coomonutillib.ui.isInternetConnectionAvalible
+import com.ar.coomonutillib.ui.showToast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val isOnline = isInternetConnectionAvalible(this@MainActivity)
+
         findViewById<Button>(R.id.clickMeBtn).setOnClickListener {
-            ARUiUtil.showToast(this@MainActivity, "Clicked")
+            showToast(this@MainActivity, "Clicked")
+
         }
     }
+
+
+//    fun String.isValidEmail() =
+//        !TextUtils.isEmpty(this) && Patterns.EMAIL_ADDRESS.matcher(this).matches()
 }
